cd8964c3461d43415a2608ab8d5bbbde22493fda
Karthikeyan Venkatesan <kn.in@live.in>
2025-12-12 19:51:40 +0530

    Add `.gitignore` with templates for common tools and initial project setup files

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const repoName = process.env.GITHUB_REPOSITORY?.split('/')?.[1]
+const isCI = !!process.env.GITHUB_ACTIONS
+const base = repoName ? `/${repoName}/` : '/'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
